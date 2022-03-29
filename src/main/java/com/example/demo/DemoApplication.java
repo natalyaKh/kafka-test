c043c188b2ec74138d9138c4d3c91b5a990755d0
roo <roo@localhost>
2022-03-29 13:22:25 +0300

    add swagger description

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,14 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "kafka-test-project" , version = "1.0", 
+description = "work with kafka", contact = @Contact(name = "Natalya", email = "bestEmail"))
+		)
 public class DemoApplication {
 
 	public static void main(String[] args) {
